711ba76ac9f900fbcac07543a6701261043810dc
JerryXu316 <934159213@qq.com>
2025-10-16 09:17:57 +0800

    Submit homework

--- homework2/code/Newton_Interpolation.cpp
+++ homework2/code/Newton_Interpolation.cpp
@@ -1,50 +1,60 @@
-#include <vector>
-using namespace std;
+#include <iostream>
+#include <vector> 
+using namespace std; 
 
 class NewtonPolynomial {
-    vector<double> coeff; // 差商系数 a0, a1, ...
-    vector<double> x;     // 节点
+private:
+    vector<double> x;
+    vector<double> coefficients;
 
 public:
-    NewtonPolynomial(const vector<double>& xs, const vector<double>& ys) {
-        int n = xs.size();
-        x = xs;
-        coeff = ys; // 初始差商表的第一列是 y 值
-
-        // 计算差商，原地更新
-        for (int j = 1; j < n; j++) {
-            for (int i = n - 1; i >= j; i--) {
-                coeff[i] = (coeff[i] - coeff[i - 1]) / (xs[i] - xs[i - j]);
-            }
-        }
-        // coeff[0..n-1] 就是 a0..an
-    }
+    NewtonPolynomial(const vector<double> &x_, const vector<double> &coeff_)
+        : x(x_), coefficients(coeff_) {}
 
     double operator()(double x0) const {
-        double res = 0;
-        int n = coeff.size();
-        // Newton 公式展开
-        for (int i = n - 1; i >= 0; i--) {
-            res = res * (x0 - x[i]) + coeff[i];
+        double result = 0.0;
+        double term = 1.0;
+        for (size_t i = 0; i < coefficients.size(); i++) {
+            result += coefficients[i] * term;
+            term *= (x0 - x[i]);
         }
-        return res;
+        return result;
     }
 };
 
-auto newtonInterp(const vector<double> &x, const vector<double> &y) {
-    return NewtonPolynomial(x, y);
-}
 
 
-#include <iostream>
+auto newtonInterp(const vector<double> &x, const vector<double> &y) { 
+    int n = x.size(); 
+    vector<double> coefficients = y; 
+    for(int i = 0; i < n - 1; i++){ 
+        for(int j = 0; j < n - i - 1; j++){ 
+            coefficients[n - j - 1] = (coefficients[n - j - 1] - coefficients[n - j - 2]) / (x[n - j - 1] - x[n - j - i - 2]);
+        } 
+    } 
+
+    return NewtonPolynomial(x, coefficients);
+
+} 
+        
+        
+/*
+auto p = newtonInterp(x, y);
+// 你需要确保评测系统可以用这种方式使用你的返回对象：通过小括号运算，获取你的插值多项式在 x0 处的取值。
+double f0 = p(x0);
+*/
 
 int main() {
-    vector<double> x = {0, 1, 2};
-    vector<double> y = {1, 3, 2};
-    auto p = newtonInterp(x, y);
+    vector<double> x = {1.0, 2.0, 3.0};
+    vector<double> y = {2.0, 3.0, 5.0};
 
-    cout << p(0) << endl; // 应该等于 1
-    cout << p(1) << endl; // 应该等于 3
-    cout << p(2) << endl; // 应该等于 2
-    cout << p(1.5) << endl; // 插值结果
+    try {
+        auto p = newtonInterp(x, y);
+        double x0 = 2.5;
+        cout << "P(" << x0 << ") = " << p(x0) << endl; // 4.25
+    } catch (const exception &e) {
+        cerr << "Error: " << e.what() << endl;
+    }
+
+    return 0;
 }
